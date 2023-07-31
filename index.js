dc6f594870b1a3c97df01366c889c75dcf3fb47e
Navneet Singh <mufc.navneet@gmail.com>
2023-07-31 21:12:56 +0530

    display the fetched data from api

--- index.js
+++ index.js
@@ -20,14 +20,57 @@ function constructURL2(e_id) {
 function doFetch1(emp) {
     fetch(constructURL1(emp))
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => {
+            console.log(json);
+            displayData(json.data);
+        })
+        .catch(error => console.error(error));
 }
 
 function doFetch2(e_id) {
     fetch(constructURL2(e_id))
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => {
+            console.log(json);
+            displayData(json.data);
+        })
+        .catch(error => console.error(error));
 }
 
 doFetch1(emp2);
 doFetch2(id_2);
+
+
+function displayData(data) {
+    var mainContainer = document.querySelector("#mydata");
+    mainContainer.innerHTML = ""; // Clears previous data if any
+
+    // Checks if the data is an array or an object and process accordingly
+    if (Array.isArray(data)) {
+        for (var i = 0; i < data.length; i++) {
+            var div = document.createElement("div");
+            div.innerHTML =
+                "id: " +
+                data[i].id + "\n" +
+                "Employee Name: " +
+                data[i].employee_name + "\n" +
+                "Salary " +
+                data[i].employee_salary + "\n" +
+                "Age " +
+                data[i].employee_age; + "\n" + "-------------------------------"
+            mainContainer.appendChild(div);
+        }
+    } else {
+        var div = document.createElement("div");
+        div.innerHTML =
+            "id: " +
+            data.id + "\n" +
+            "Employee Name: " +
+            data.employee_name + "\n" +
+            "Salary " +
+            data.employee_salary + "\n" +
+            "Age " +
+            data.employee_age; + "\n" + "-------------------------------"
+        mainContainer.appendChild(div);
+    }
+}
