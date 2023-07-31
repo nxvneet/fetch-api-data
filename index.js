22496924cc6f96892487a6eb36859c73dea918ed
Navneet Singh <mufc.navneet@gmail.com>
2023-07-31 19:55:51 +0530

    fetching done

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=";
+var url1 = "https://dummy.restapiexample.com/api/v1/employees"; // this api gives all data by on call
+var url2 = "https://dummy.restapiexample.com/api/v1/employee/"; // this api gives data of 1 employee only [ employee/{id} ]
 
-const text1 = "I am navv";
-const text2 = "API fetching successfull!";
-const text3 = "text 3 also fetched!"
+const emp1 = "Airi Satou";
+const emp2 = "Cedric Kelly";
+const id_1 = 10;
+const id_2 = 20;
+
+function constructURL1(emp) {
+
+    return url1 + "?" + "employee_name=" + emp;
+}
 
 
-function constructURL(text) {
+function constructURL2(e_id) {
 
-    fetch(url.text)
+    return url2 + e_id;
+}
+
+function doFetch1(emp) {
+    fetch(constructURL1(emp))
         .then(response => response.json())
-        .then(json => console.log(json));
-
+        .then(json => console.log(json))
 }
 
-function displayURL(text) {
-    console.log(constructURL(url + text));
+function doFetch2(e_id) {
+    fetch(constructURL2(e_id))
+        .then(response => response.json())
+        .then(json => console.log(json))
 }
 
-displayURL(text1);
+doFetch1(emp2);
+doFetch2(id_2);
